70557ca3a8cf381d6e98dc432da846c36ba26bb0
rohithnafeel <devarc16@gmail.com>
2026-07-09 22:48:06 +0530

    Added side auto dock system

--- Engine/Editor/EditorLayer.cpp
+++ Engine/Editor/EditorLayer.cpp
@@ -1,6 +1,9 @@
 #include "Arc/Editor/EditorLayer.h"
 
 #include <imgui.h>
+#include <imgui_internal.h>
+
+
 #pragma message("Using ImGui version: " IMGUI_VERSION)
 
 namespace Arc
@@ -10,69 +13,130 @@ namespace Arc
     }
 
     void EditorLayer::Render(unsigned int framebufferTexture)
-{
-    if (ImGui::BeginMainMenuBar())
     {
-        if (ImGui::BeginMenu("File"))
-        {
-            ImGui::MenuItem("New");
-            ImGui::MenuItem("Open");
-            ImGui::MenuItem("Save");
-            ImGui::Separator();
-            ImGui::MenuItem("Exit");
-            ImGui::EndMenu();
-        }
 
-        if (ImGui::BeginMenu("Edit"))
-        {
-            ImGui::MenuItem("Undo");
-            ImGui::MenuItem("Redo");
-            ImGui::EndMenu();
-        }
+        ImGuiWindowFlags window_flags =
+            ImGuiWindowFlags_MenuBar |
+            ImGuiWindowFlags_NoDocking;
 
-        if (ImGui::BeginMenu("View"))
-        {
-            ImGui::MenuItem("Viewport");
-            ImGui::MenuItem("Hierarchy");
-            ImGui::MenuItem("Inspector");
-            ImGui::EndMenu();
-        }
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
 
-        if (ImGui::BeginMenu("Help"))
-        {
-            ImGui::MenuItem("About");
-            ImGui::EndMenu();
-        }
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
 
-        ImGui::EndMainMenuBar();
-    }
+        window_flags |= ImGuiWindowFlags_NoTitleBar;
+        window_flags |= ImGuiWindowFlags_NoCollapse;
+        window_flags |= ImGuiWindowFlags_NoResize;
+        window_flags |= ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+        window_flags |= ImGuiWindowFlags_NoNavFocus;
 
-    ImGui::Begin("Viewport");
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
 
-    ImVec2 viewportSize = ImGui::GetContentRegionAvail();
+        ImGui::Begin("DockSpace", nullptr, window_flags);
 
-    ImGui::Image(
-        (ImTextureID)(intptr_t)framebufferTexture,
-        viewportSize,
-        ImVec2(0, 1),
-        ImVec2(1, 0)
+        ImGui::PopStyleVar(2);
+
+        ImGuiID dockspaceID = ImGui::GetID("ArcDockSpace");
+
+        ImGui::DockSpace(
+            dockspaceID,
+            ImVec2(0.0f, 0.0f),
+            ImGuiDockNodeFlags_None
+        );
+
+        static bool firstTime = true;
+
+if (firstTime)
+{
+    firstTime = false;
+
+    ImGui::DockBuilderRemoveNode(dockspaceID);
+    ImGui::DockBuilderAddNode(
+        dockspaceID,
+        ImGuiDockNodeFlags_DockSpace
     );
 
-    ImGui::End();
+    ImGui::DockBuilderSetNodeSize(
+        dockspaceID,
+        viewport->WorkSize
+    );
 
-    ImGui::Begin("Hierarchy");
-    ImGui::BulletText("Camera");
-    ImGui::BulletText("Triangle");
-    ImGui::End();
+    ImGuiID dock_main = dockspaceID;
 
-    ImGui::Begin("Inspector");
-    ImGui::Text("No object selected.");
-    ImGui::End();
+    ImGuiID dock_left;
+    ImGuiID dock_right;
+    ImGuiID dock_bottom;
 
-    ImGui::Begin("Console");
-    ImGui::Text("Arc Engine started successfully.");
-    ImGui::End();
 }
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                ImGui::MenuItem("New");
+                ImGui::MenuItem("Open");
+                ImGui::MenuItem("Save");
+                ImGui::Separator();
+                ImGui::MenuItem("Exit");
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Edit"))
+            {
+                ImGui::MenuItem("Undo");
+                ImGui::MenuItem("Redo");
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("View"))
+            {
+                ImGui::MenuItem("Viewport");
+                ImGui::MenuItem("Hierarchy");
+                ImGui::MenuItem("Inspector");
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Help"))
+            {
+                ImGui::MenuItem("About");
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+
+        ImGui::Begin("Viewport");
+
+        ImVec2 viewportSize = ImGui::GetContentRegionAvail();
+
+        ImGui::Image(
+            (ImTextureID)(intptr_t)framebufferTexture,
+            viewportSize,
+            ImVec2(0, 1),
+            ImVec2(1, 0)
+        );
+
+        ImGui::End();
+
+        ImGui::Begin("Hierarchy");
+        ImGui::BulletText("Camera");
+        ImGui::BulletText("Triangle");
+        ImGui::End();
+
+        ImGui::Begin("Inspector");
+        ImGui::Text("No object selected.");
+        ImGui::End();
+
+        ImGui::Begin("Console");
+        ImGui::Text("Arc Engine started successfully.");
+        ImGui::End();
+
+        // Close DockSpace
+        ImGui::End();
+    }
+
     void EditorLayer::End()
     {
     }
